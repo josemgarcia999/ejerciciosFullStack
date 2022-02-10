fe48fc8703d009b1776743206a33655c76665203
josemanuel.garcia <josemanuel.garcia@bosonit.com>
2022-02-10 10:28:05 +0100

    Creando proyecto Maven

---
+++ Ejercicio2/src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+  public static void main(String[] args) {
+    ArchivoDeDatos a = new ArchivoDeDatos();
+    a.lectura("C:\\Users\\josemanuel.garcia\\Desktop\\Proyectos\\GitHub\\ejerciciosFullStack\\prueba.txt");
+
+  }
+}
