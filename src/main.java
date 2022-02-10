2c9c765b23ce249dde3e982ebe60a328a437a721
josemanuel.garcia <josemanuel.garcia@bosonit.com>
2022-02-10 09:28:42 +0100

    Añadir fichero prueba.txt al proyecto

--- src/main.java
+++ src/main.java
@@ -1,6 +1,6 @@
 public class main {
   public static void main(String[] args) {
     ArchivoDeDatos a = new ArchivoDeDatos();
-    a.lectura("C:\\Users\\josemanuel.garcia\\IdeaProjects\\prueba.txt");
+    a.lectura("C:\\Users\\josemanuel.garcia\\IdeaProjects\\ejerciciosFullStack\\prueba.txt");
   }
 }
