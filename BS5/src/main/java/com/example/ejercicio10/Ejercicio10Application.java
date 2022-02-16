b6ff98fedf09abea69f74de66f3dfebf2f0ad716
josemanuel.garcia <josemanuel.garcia@bosonit.com>
2022-02-16 13:50:36 +0100

    Inicio BS5

--- BS5/src/main/java/com/example/ejercicio10/Ejercicio10Application.java
+++ BS5/src/main/java/com/example/ejercicio10/Ejercicio10Application.java
@@ -1,13 +1,26 @@
 package com.example.ejercicio10;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
+@Slf4j
 public class Ejercicio10Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ejercicio10Application.class, args);
-	}
 
+		SpringApplication.run(Ejercicio10Application.class, args);
+
+	}
+	@PostConstruct
+	private void pruebaLog(){
+		log.trace("A TRACE Message");
+		log.debug("A DEBUG Message");
+		log.info("An INFO Message");
+		log.warn("A WARN Message");
+		log.error("An ERROR Message");
+	}
 }
