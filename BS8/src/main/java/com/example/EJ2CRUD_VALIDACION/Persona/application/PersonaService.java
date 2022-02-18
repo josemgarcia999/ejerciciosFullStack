7e56422960b4c5228aa2cdb4ac54a2dd429dcbbd
josemanuel.garcia <josemanuel.garcia@bosonit.com>
2022-02-18 13:35:34 +0100

    BS8 con duda sobre el paquete de las excepciones

---
+++ BS8/src/main/java/com/example/EJ2CRUD_VALIDACION/Persona/application/PersonaService.java
@@ -0,0 +1,106 @@
+package com.example.EJ2CRUD_VALIDACION.Persona.application;
+
+import com.example.EJ2CRUD_VALIDACION.Persona.exceptions.NotFoundException;
+import com.example.EJ2CRUD_VALIDACION.Persona.domain.PersonaEntity;
+import com.example.EJ2CRUD_VALIDACION.Persona.infraestructure.controller.dto.imput.PersonaImputDTO;
+import com.example.EJ2CRUD_VALIDACION.Persona.infraestructure.controller.dto.output.PersonaOutputDTO;
+import com.example.EJ2CRUD_VALIDACION.Persona.infraestructure.repository.jpa.PersonaRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class PersonaService implements IPersona {
+
+  @Autowired PersonaRepo personaRepo;
+
+  @Override
+  public PersonaOutputDTO añadirUsuario(PersonaImputDTO persona) throws Exception {
+    persona.comprobarDTO();
+    PersonaEntity p = new PersonaEntity(persona);
+    personaRepo.save(p);
+    PersonaOutputDTO personaOutputDTO = new PersonaOutputDTO(p);
+    return personaOutputDTO;
+  }
+
+  @Override
+  public PersonaOutputDTO findById(Integer id) throws Exception {
+    PersonaEntity p =   personaRepo.findById(id).orElseThrow(() -> new NotFoundException("Usuario con Id: "+id +" no encontrado"));
+    PersonaOutputDTO personaOutputDTO = new PersonaOutputDTO(p);
+    return personaOutputDTO;
+  }
+
+  @Override
+  public List<PersonaOutputDTO> getAllPersonas() {
+    List<PersonaEntity> personasEntities = personaRepo.findAll();
+    List<PersonaOutputDTO> personas = new ArrayList<>();
+    for (PersonaEntity p : personasEntities) {
+      PersonaOutputDTO per = new PersonaOutputDTO(p);
+      personas.add(per);
+    }
+    return personas;
+  }
+
+  @Override
+  public List<PersonaOutputDTO> findByUsuario(String usuario) {
+    List<PersonaEntity> personas = personaRepo.findAll();
+    List<PersonaOutputDTO> personaOutputDTOS = new ArrayList<>();
+    for (PersonaEntity p : personas) {
+      if (p.getUsuario().equalsIgnoreCase(usuario)) {
+        PersonaOutputDTO persona = new PersonaOutputDTO(p);
+        personaOutputDTOS.add(persona);
+      }
+    }
+    return personaOutputDTOS;
+  }
+
+  @Override
+  public void deletePersona(Integer id) throws Exception {
+    personaRepo.delete(
+        personaRepo.findById(id).orElseThrow(() -> new NotFoundException("Usuario con Id: "+id +" no encontrado")));
+  }
+
+  @Override
+  public PersonaOutputDTO updatePersona(Integer id, PersonaImputDTO personaImputDTO) throws Exception {
+
+    PersonaEntity p = personaRepo.findById(id).orElseThrow(() -> new NotFoundException("Usuario con Id: "+id +" no encontrado"));
+    if (personaImputDTO.getUsuario().length() < 6 || personaImputDTO.getUsuario().length() > 10) {
+      p.setUsuario(personaImputDTO.getUsuario());
+    }
+
+    if (personaImputDTO.getPassword() != null) {
+      p.setPassword(personaImputDTO.getPassword());
+    }
+    if (personaImputDTO.getName() != null) {
+      p.setName(personaImputDTO.getName());
+    }
+    if (personaImputDTO.getSurname() != null) {
+      p.setSurname(personaImputDTO.getSurname());
+    }
+    if (personaImputDTO.getCompany_email() != null) {
+      p.setCompany_email(personaImputDTO.getCompany_email());
+    }
+    if (personaImputDTO.getPersonal_email() != null) {
+      p.setPersonal_email(personaImputDTO.getPersonal_email());
+    }
+    if (personaImputDTO.getCity() != null) {
+      p.setCity(personaImputDTO.getCity());
+    }
+    if (personaImputDTO.getActive() != null) {
+      p.setActive(personaImputDTO.getActive());
+    }
+    if (personaImputDTO.getImagen_url() != null) {
+      p.setImagen_url(personaImputDTO.getImagen_url());
+    }
+
+    if (personaImputDTO.getTermination_date() != null) {
+      p.setTermination_date(personaImputDTO.getTermination_date());
+    }
+
+    personaRepo.saveAndFlush(p);
+    PersonaOutputDTO personaOutputDTO = new PersonaOutputDTO(p);
+    return personaOutputDTO;
+  }
+}
---
+++ BS8/src/main/java/com/example/EJ2CRUD_VALIDACION/Persona/infraestructure/controller/dto/output/PersonaOutputDTO.java
@@ -0,0 +1,46 @@
+package com.example.EJ2CRUD_VALIDACION.Persona.infraestructure.controller.dto.output;
+
+import com.example.EJ2CRUD_VALIDACION.Persona.domain.PersonaEntity;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class PersonaOutputDTO {
+    
+    Integer id;
+    String usuario;
+    String password;
+    String name;
+    String surname;
+    String company_email;
+    String personal_email;
+    String city;
+    Boolean active;
+    Date created_date;
+    String imagen_url;
+    Date termination_date;
+
+
+    public PersonaOutputDTO (PersonaEntity personaEntity){
+        if(personaEntity == null)
+            return;
+        setId(personaEntity.getId());
+        setUsuario(personaEntity.getUsuario());
+        setPassword(personaEntity.getPassword());
+        setName(personaEntity.getName());
+        setSurname(personaEntity.getSurname());
+        setCompany_email(personaEntity.getCompany_email());
+        setPersonal_email(personaEntity.getPersonal_email());
+        setCity(personaEntity.getCity());
+        setActive(personaEntity.getActive());
+        setCreated_date(personaEntity.getCreated_date());
+        setImagen_url(personaEntity.getImagen_url());
+        setTermination_date(personaEntity.getTermination_date());
+
+    }
+
+    public PersonaOutputDTO() {
+
+    }
+}
